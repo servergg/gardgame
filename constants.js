1057a34928fd188c7e15d68430453646feae1184
Ricardo Lino <ricardo.linorj@gmail.com>
2022-11-17 18:27:38 +0000

    split code into files

---
+++ constants.js
@@ -0,0 +1,93 @@
+import { average, range } from "./utils.js";
+
+export const MAX_STAT_VALUE = 9;
+export const MIN_STAT_VALUE = 0;
+export const MEAN_STAT_VALUE = average(range(MIN_STAT_VALUE, MAX_STAT_VALUE));
+export const STATS = ["STR", "DEX", "CON", "WIS", "INT", "CHA"];
+export const ELEMENTS = [
+    { name: "fire", wins: [1, 3] },
+    { name: "earth", wins: [2, 4] },
+    { name: "water", wins: [0, 3] },
+    { name: "wind", wins: [4, 1] },
+    { name: "lighting", wins: [0, 2] }
+];
+export const BUILDS = [
+    {
+        name: "mage", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+        statsDistribution: [-0.5, 0, -0.5, 0.5, 0.5, 0] // the sum of all elements must be 0
+    },
+    {
+        name: "warrior", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+        statsDistribution: [0.5, 0, 0.5, -0.5, -0.5, 0]
+    },
+    {
+        name: "cleric", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+        statsDistribution: [0, -0.5, 0.5, 0, -0.5, 0.5]
+    },
+    {
+        name: "rogue", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+        statsDistribution: [-0.5, 0.5, -0.5, 0, 0, 0.5]
+    },
+    {
+        name: "ranger", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+        statsDistribution: [0, 0.5, 0, 0.5, -0.5, -0.5]
+    },
+    // {
+    //     name: "battle mage", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+    //     statsDistribution: [1, 1, 1, 1, 1, 1]
+    // },
+    // {
+    //     name: "warlock", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+    //     statsDistribution: [1, 1, 1, 1, 1, 1]
+    // },
+    // {
+    //     name: "trickster", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+    //     statsDistribution: [1, 1, 1, 1, 1, 1]
+    // },
+    // {
+    //     name: "paladin", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+    //     statsDistribution: [1, 1, 1, 1, 1, 1]
+    // },
+    // {
+    //     name: "swashbuckler", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+    //     statsDistribution: [1, 1, 1, 1, 1, 1]
+    // },
+    // {
+    //     name: "druid", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+    //     statsDistribution: [1, 1, 1, 1, 1, 1]
+    // },
+    // {
+    //     name: "shaman", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+    //     statsDistribution: [1, 1, 1, 1, 1, 1]
+    // },
+    // {
+    //     name: "barbarian", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+    //     statsDistribution: [1, 1, 1, 1, 1, 1]
+    // },
+    // {
+    //     name: "inquisitor", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+    //     statsDistribution: [1, 1, 1, 1, 1, 1]
+    // },
+    // {
+    //     name: "ninja", // ["STR", "DEX", "CON", "WIS", "INT", "CHA"]
+    //     statsDistribution: [1, 1, 1, 1, 1, 1]
+    // },
+];
+
+export const NAMES = [
+    "Aeore (Ary Roberto)",
+    "anainthesky",
+    "Bwana Plemos",
+    "gabrieleiro",
+    "Germano",
+    "Katsuo",
+    "Kriz",
+    "Lino",
+    "lucas",
+    "Nham",
+    "nobusada",
+    "Schweins",
+    "Trilles",
+    "𝖇𝖗𝖊𝖓𝖔",
+    ":skull::fire::skull:RenSound :skull::fire::skull:",
+];
--- index.js
+++ index.js
@@ -1,139 +1,55 @@
-const MAX_STAT_VALUE = 99;
-const MIN_STAT_VALUE = 0;
-const STATS = ["STR", "DEX", "CON", "WIS", "INT", "CHA"];
-const ELEMENTS = [
-    { name: "fire", wins: [1, 3] },
-    { name: "earth", wins: [2, 4] },
-    { name: "water", wins: [0, 3] },
-    { name: "wind", wins: [4, 1] },
-    { name: "lighting", wins: [0, 2] }
-];
-const BUILDS = [
-    {
-        name: "mage", 
-        statsDistribution: [1.5, 1, 1.5, 0.5, 0.5, 1] // the sum of all elements must be 6
-    },
-    {
-        name: "battle mage", 
-        statsDistribution: [0.5, 1, 0.5, 1.5, 1.5, 1]
-    },
-    {
-        name: "warlock", 
-        statsDistribution: [0.5, 1, 0.5, 1.5, 1.5, 1]
-    },
-    {
-        name: "trickster", 
-        statsDistribution: [0.5, 1, 0.5, 1.5, 1.5, 1]
-    },
-    {
-        name: "warrior", 
-        statsDistribution: [0.5, 1, 0.5, 1.5, 1.5, 1]
-    },
-    {
-        name: "paladin", 
-        statsDistribution: [0.5, 1, 0.5, 1.5, 1.5, 1]
-    },
-    {
-        name: "swashbuckler", 
-        statsDistribution: [0.5, 1, 0.5, 1.5, 1.5, 1]
-    },
-    {
-        name: "cleric", 
-        statsDistribution: [1, 1.5, 0.5, 1, 1.5, 0.5]
-    },
-    {
-        name: "druid", 
-        statsDistribution: [0.5, 1, 0.5, 1.5, 1.5, 1]
-    },
-    {
-        name: "shaman", 
-        statsDistribution: [0.5, 1, 0.5, 1.5, 1.5, 1]
-    },
-    {
-        name: "barbarian", 
-        statsDistribution: [0.5, 1, 0.5, 1.5, 1.5, 1]
-    },
-    {
-        name: "inquisitor", 
-        statsDistribution: [1, 1.5, 0.5, 1, 1.5, 0.5]
-    },
-    {
-        name: "rogue", 
-        statsDistribution: [1.5, 0.5, 1.5, 1, 1, 0.5]
-    },
-    {
-        name: "ninja", 
-        statsDistribution: [1.5, 0.5, 1.5, 1, 1, 0.5]
-    },
-    {
-        name: "ranger", 
-        statsDistribution: [1, 1.5, 0.5, 1, 1.5, 0.5]
-    },
-];
-
-function getRandomDistributedNumber (min, max, skew) {
-    let u = 0, v = 0;
-    while(u === 0) u = Math.random() //Converting [0,1) to (0,1)
-    while(v === 0) v = Math.random()
-    let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )
-    
-    num = num / 10.0 + 0.5 // Translate to 0 -> 1
-    if (num > 1 || num < 0) 
-      num = getRandomDistributedNumber(min, max, skew) // resample between 0 and 1 if out of range
-    
-    else{
-      num = Math.pow(num, skew) // Skew
-      num *= max - min // Stretch to fill range
-      num += min // offset to min
-    }
-    return num
-};
+import {
+    MAX_STAT_VALUE,
+    MIN_STAT_VALUE,
+    MEAN_STAT_VALUE,
+    STATS,
+    ELEMENTS,
+    BUILDS
+} from "./constants.js";
+import { randomTruncSkewNormal } from "./utils.js";
 
 function getRandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-  }
+}
 
-function generateStat (skew = 1) {
-    const value = getRandomDistributedNumber(MIN_STAT_VALUE, MAX_STAT_VALUE, skew);
-    return Math.floor(value * 0.1);
+function generateStat(skew = 0) {
+    const range = [MIN_STAT_VALUE, MAX_STAT_VALUE];
+    const value = randomTruncSkewNormal({ mean: MEAN_STAT_VALUE, stdDev: 2, range, skew });
+    return Math.round(value);
 };
 
-function getRandomElement () {
+function getRandomElement() {
     const index = getRandomNumber(0, ELEMENTS.length);
 
     return index;
 }
 
-function getRandomBuild () {
+function getRandomBuild() {
     const index = getRandomNumber(0, BUILDS.length);
 
     return index;
 }
 
-function generateCard () {
+function generateCard() {
     var buildIndex = getRandomBuild();
     var elementIndex = getRandomElement();
 
     var elementName = ELEMENTS[elementIndex].name;
     var className = BUILDS[buildIndex].name;
     var statsNumber = BUILDS[buildIndex].statsDistribution.map(value => generateStat(value));
+    var statsRank = statsNumber.reduce((a, v, i) => ({ ...a, [STATS[i]]: v }), {});
 
-    var statsRank = statsNumber.reduce((a, v, i) => {
-        if (v === 9) console.log({
-            elementName,
-            className,
-            statsNumber,
-        });
-        return { ...a, [STATS[i]]: v}
-    }, {})
+    // statsNumber.find(v => {
+    //     if (v === 9) {
+    //         console.log({ elementName, className, statsRank });
+    //         return true;
+    //     }
 
-        // console.log({
-        //     elementName,
-        //     className,
-        //     statsRank,
-        // });
+    //     return false;
+    // });
+    console.log({ elementName, className, statsRank });
 }
 
 console.log([...Array(10).keys()].forEach(generateCard))
