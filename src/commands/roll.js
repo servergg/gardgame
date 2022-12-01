76a7877646b2748d305da2810937e18794d12b1a
Gabriel Thomé <38537062+g-thome@users.noreply.github.com>
2022-12-01 01:07:19 +0000

    chore: remove unused imports

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -1,7 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 const { generateCard } = require('./../index.js');
 const { Cards } = require('../db/memory/Cards')
-const { os } = require('os')
 const http = require("http");
 
 
